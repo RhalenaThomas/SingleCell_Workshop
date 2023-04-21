dc9a976107cef4066e5a9588d2ddf242ea272944
RhalenaThomas <rhalena.t@gmail.com>
2023-04-21 09:33:49 -0400

    adding annotation

---
+++ InstallPackages.R
@@ -0,0 +1,18 @@
+# install required packages
+# R version must be  >= 4.2
+
+# install devtools
+
+# install bioconductor/CRAN packages
+install.packages("tidyverse")
+install.packages("Seurat")
+install.packages("remotes")
+install.packages("ggplot2")
+
+# install packages from github
+remotes::install_github('chris-mcginnis-ucsf/DoubletFinder')
+install_github("wjawaid/enrichR")
+library(devtools)
+devtools::install_github("SydneyBioX/scClassify")
+
+
