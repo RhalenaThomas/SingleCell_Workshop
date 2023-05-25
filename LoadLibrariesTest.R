bf49eaeb2814ed1f8cfb77b3a70567b35040957c
RhalenaThomas <rhalena.t@gmail.com>
2023-05-25 17:59:13 -0400

    testing workbooks

---
+++ LoadLibrariesTest.R
@@ -0,0 +1,19 @@
+# load packages
+
+library(tidyverse)
+library(Seurat)
+library(SingleCellExperiment)
+library(MetaNeighbor)
+library(anndata)
+library(SeuratDisk)
+library(Matrix)
+library(scclusteval)
+library(mclust)
+library(Seurat)
+library(tidyverse)
+library(enrichR)
+library(ggplot2)
+library("scClassify")
+library(SingleCellExperiment)
+library("Matrix")
+
