2f283348a6a7a453017c757088f7c941a0361edf
RhalenaThomas <rhalena.t@gmail.com>
2024-05-10 10:03:52 -0400

    llp' p]

--- RhalenaNoteOnPackageVersion.R
+++ RhalenaNoteOnPackageVersion.R
@@ -4,6 +4,15 @@
 # View current library paths
 .libPaths()
 
+
+# Check the current library paths
+old_libpaths <- .libPaths()
+
+# Set your primary library path
+second_libpath <- "/Users/rhalenathomas/Library/CustomR"
+.libPaths(c(old_libpaths, second_libpath))
+
+
 #[1] "/Library/Frameworks/R.framework/Versions/4.2/Resources/library"
 
 
@@ -13,6 +22,8 @@
 
 # Install Seurat 5 to a separate library location
 install.packages("Seurat", lib = "/Users/rhalenathomas/Library/CustomR")
+install.packages("SeuratObject", lib = "/Users/rhalenathomas/Library/CustomR")
+
 
 # Load Seurat 5 from the custom library location
 library(Seurat, lib.loc = "/Users/rhalenathomas/Library/CustomR/")
@@ -20,3 +31,4 @@ library(Seurat, lib.loc = "/Users/rhalenathomas/Library/CustomR/")
 
 # Check Seurat version
 packageVersion("Seurat")
+packageVersion("SeuratObject")
