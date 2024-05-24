f4b1890db1c637eadb41b5d703ca9369e54a771d
RhalenaThomas <rhalena.t@gmail.com>
2024-05-24 07:58:51 -0400

    actually updated workbook same OG name

--- WorkshopPackagesInstallation.R
+++ WorkshopPackagesInstallation.R
@@ -12,7 +12,7 @@ old_libpaths <- .libPaths()
 # Set your primary library path
 second_libpath <- "/Users/rhalenathomas/Library/CustomR"
 .libPaths(c(old_libpaths, second_libpath))
-
+.libPaths(c(second_libpath,old_libpaths))
 #### if you want to install everything in to your normal library 
 # run 
 #second_libpath <- .libPaths()
@@ -27,10 +27,39 @@ install.packages("remotes", lib = second_libpath)
 install.packages("ggplot2", lib = second_libpath)
 install.packages("patchwork", lib = second_libpath)
 install.packages("clustree", lib = second_libpath)
+install.packages("anndata", lib = second_libpath)
+
 
 remotes::install_github('chris-mcginnis-ucsf/DoubletFinder', lib = second_libpath)  ### required some kind of password chain on my mac 
 remotes::install_github("wjawaid/enrichR", lib = second_libpath)
 remotes::install_github("SydneyBioX/scClassify", lib = second_libpath)
+
+
+if (!require("BiocManager", quietly = TRUE))
+  install.packages("BiocManager")
+
+BiocManager::install("SingleCellExperiment", lib = second_libpath, force = TRUE)
+
+
+# note "Matrix" should be installed from base R but it isn't in my separate library. 
+
+library(SingleCellExperiment, lib = second_libpath)
+
+library(readr)
+library(MetaNeighbor)
+library(anndata)
+library(Matrix)
+library(scclusteval)
+library(mclust)
+library(MAST)
+library(EnhancedVolcano)
+library(DESeq2)
+
+
+
+
+
+
 
 
 # Load Seurat 5 from the custom library location
@@ -43,6 +72,8 @@ library(clustree, lib.loc = second_libpath)
 library(DoubletFinder, lib.loc = second_libpath)
 library(enrichR, lib = second_libpath)
 library(scClassify, lib = second_libpath)
+library(anndata, lib = second_libpath)
+library(Matrix)
 
 # Check Seurat version
 packageVersion("Seurat")
